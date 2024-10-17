f298ddd9fca60adcef931314e19b51d358dd148c
ZRD <65111609+Zordon1337@users.noreply.github.com>
2024-10-17 22:44:27 +0200

    added login js

---
+++ score-server/src/web/js/login.js
@@ -0,0 +1,13 @@
+function Register(username, password) {
+    fetch(`api/v1/login?u=${username}&p=${password}`)
+    .then(response => response.text())
+    .then(data => {
+       if(data.startsWith("ERR")) {
+            var dat = data.split('\n')
+            alert(dat[1])
+       } else {
+        alert("Successfully logged in!")
+       }
+    })
+    .catch(error => console.error('Error logging in', error));
+}
