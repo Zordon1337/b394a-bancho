bb2f89e6ee8855cc00927444fcc20a39d69ff69f
ZRD <65111609+Zordon1337@users.noreply.github.com>
2024-10-17 22:48:48 +0200

    added login endpoint

---
+++ score-server/src/Handlers/api/login.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+	"score-server/src/Utils"
+	"score-server/src/db"
+)
+
+func HandleLogin(w http.ResponseWriter, r *http.Request) {
+	ses, err := Utils.Sessions.Get(r, "usersession")
+	if err != nil {
+		fmt.Fprintf(w, "-1")
+		return
+	}
+	if r.URL.Query().Get("u") != "" && r.URL.Query().Get("p") != "" {
+		user := r.URL.Query().Get("u")
+		pass := r.URL.Query().Get("p")
+		res := db.IsCorrectCred(user, pass)
+		if res {
+			ses.Values["username"] = user
+			ses.Values["password"] = pass
+		} else {
+
+			delete(ses.Values, "username")
+			delete(ses.Values, "password")
+			fmt.Fprintf(w, "ERR\nWrong username or password")
+			return
+		}
+		ses.Save(r, w)
+	}
+	fmt.Fprintf(w, "SUCCESS")
+}
