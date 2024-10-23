cc561f0f862394ac961a5bd387dd122091c48394
ZRD <65111609+Zordon1337@users.noreply.github.com>
2024-10-23 23:18:44 +0200

    Added profiles

--- score-server/src/web/js/profile.js
+++ score-server/src/web/js/profile.js
@@ -1,12 +1,17 @@
-function handleHashRoute() {
-    const hash = window.location.hash;
-    if (hash.startsWith("#/player/")) {
-        const playerId = hash.split("#/player/")[1];
+document.addEventListener('DOMContentLoaded', async function() {
+    const hash = window.location.href;
+    if (window.location.href.includes("/profile/")) {
+        const playerId = hash.split("/profile/")[1];
+        var fet = await fetch(`/api/v1/findplayer/${playerId}`)
+        var response = await fet.json()
+        document.getElementById("Username").textContent = `${response.Username} (#${response.Rank})`
+        document.getElementById("RankedScore").textContent = `Ranked Score: ${response.RankedScore}`
+        document.getElementById("TotalScore").textContent = `Total Score: ${response.TotalScore}`
+        document.getElementById("Accuracy").textContent = `Accuracy: ${response.Accuracy*100}%`
+        document.getElementById("PlayCount").textContent = `Play Count: ${response.PlayCount}`
+        document.getElementById("JoinDate").textContent = `Join Date: ${response.JoinDate}`
+        document.getElementById("LastOnline").textContent = `Last Online: ${response.LastOnline}`
     } else {
         
     }
-}
-
-window.addEventListener('hashchange', handleHashRoute);
-
-handleHashRoute();
+});
