cf795f1d369d50527186d2f1c9176a73b3b55481
ZRD <65111609+Zordon1337@users.noreply.github.com>
2024-10-26 22:29:56 +0200

    oopsie

--- server/frontend/src/Handlers/api/PfpUpload.go
+++ server/frontend/src/Handlers/api/PfpUpload.go
@@ -29,7 +29,7 @@ func PfpUpload(w http.ResponseWriter, r *http.Request) {
 					return
 				}
 				defer file.Close()
-				destFile, err := os.Create("./Avatars/" + strconv.Itoa(int(db.GetUserIdByUsername(username))) + ".png")
+				destFile, err := os.Create("./frontend/Avatars/" + strconv.Itoa(int(db.GetUserIdByUsername(username))) + ".png")
 				if err != nil {
 					Utils.LogErr("Unable to create destination file")
 					return
